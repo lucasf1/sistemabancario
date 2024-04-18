3f5600fb1e0070512938d79389c80253bce058cc
Lucas Farias <24313990-LucasFarias38@users.noreply.replit.com>
2024-04-18 23:13:31 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,24 @@
+menu = """
+    Digite o número da opção desejada:
+    
+    1 - Para Depositar
+    2 - Para Sacar
+    3 - Para Extrato
+    4 - Para Sair
+"""
+
+while True:
+    opcao = int(input(menu))
+
+    if opcao == 1:
+        pass
+    elif opcao == 2:
+        pass
+    elif opcao == 3:
+        pass
+    elif opcao == 4:
+        print('Agradecemos a preferência. Até a próxima!')
+        break
+    else:
+        print('Opção inválida. Tente novamente.')
+        continue
